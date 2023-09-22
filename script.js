729d6f2ae54f1f1552e3e33cfb97484c66a6fec0
Parorenger <marcos@brazil-server.com>
2023-09-22 14:24:38 -0300

    replace display picture

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ function toggleMode() {
   let img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "assets/avatar-light.png")
+    img.setAttribute("src", "assets/foto-clara-circular.png")
     img.setAttribute(
       "alt",
-      "Foto de Mayk Brito sorrindo, usando óculos escuros e jaqueta, e fundo roxo e azul."
+      "Foto de Marcos Santana sorrindo na Liberdade, usando óculos escuros e blusa clara."
     )
   } else {
-    img.setAttribute("src", "assets/avatar.png")
+    img.setAttribute("src", "assets/foto-circular.png")
     img.setAttribute(
       "alt",
-      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo."
+      "Foto de Marcos Santana sorrindo, usando terno e gravata e camisa branca e fundo amarelo."
     )
   }
 }
