6dc62b51413caf6836c6f5a6332496faf6e9cf54
Parorenger <marcos@brazil-server.com>
2023-09-28 14:54:53 -0300

    Change languages

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
+  let portfolio = document.getElementById('portfolio');
+  let twitter = document.getElementById('xprofile');
+  let explorer = document.getElementById('visit-explorer')
+
+
   let img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
@@ -10,11 +15,21 @@ function toggleMode() {
       "alt",
       "Foto de Marcos Santana sorrindo na Liberdade, usando óculos escuros e blusa clara."
     )
+    // Mudando a lingua dos itens
+    portfolio.innerHTML = "Access my portfolio on Github"
+    twitter.innerHTML = "Visit my profile on X"
+    explorer.innerHTML = "Discover more on Explorer"
+    // Fim da mudança de lingua
   } else {
     img.setAttribute("src", "assets/foto-circular.png")
     img.setAttribute(
       "alt",
       "Foto de Marcos Santana sorrindo, usando terno e gravata e camisa branca e fundo amarelo."
     )
+    // Mudando a lingua dos itens
+    portfolio.innerHTML = "Ver meu portfólio"
+    twitter.innerHTML = "Acesse meu perfil no X"
+    explorer.innerHTML = "Conheça o Explorer"
+    // Fim da mudança de lingua
   }
 }
